7b3caa3a7989949e05edc3ab37c3221e9e6f867f
Thomas-Malcolm <ThomasMalcolm01@gmail.com>
2020-03-01 20:32:38 +1000

    Changing from client to bot based events

--- src/bot.py
+++ src/bot.py
@@ -1,14 +1,16 @@
 import os
 
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
+
 API_KEY = os.getenv('DISCORD_TOKEN')
 SERVER_NAME = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
-
+mrRobot = commands.Bot(command_prefix="!")
 
 
 @client.event
@@ -19,9 +21,11 @@ async def on_ready():
 	print(f'{client.user} is connected to: ' + f'{guild.name}')
 
 
-	members = '\n - '.join([member.name for member in guild.members])
+@mrRobot.command(name="test")
+async def test_command(ctx):
+	response = "This is a test message. :hellothere:"
 
-	print(f'Members:\n - {members}')
+	await ctx.send(response)
 
 
 client.run(API_KEY)

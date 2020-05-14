e24f5ba748cac8a0b0a8d3e0c94973e7a8a7ed35
Thomas-Malcolm <ThomasMalcolm01@gmail.com>
2020-05-14 15:32:59 +1000

    Added defcon to competitions commands

--- src/commands/competitions.py
+++ src/commands/competitions.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 import datetime
 
-
+'''NOTE: given times are in UTC (most CTFs seem to be planned in it)'''
 class Competitions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -16,7 +16,7 @@ class Competitions(commands.Cog):
         if waiting.seconds < 0:
             msg = "Uhhhhh, you may have missed it...\n"
         else:
-            msg = f"There are {waiting.days} days, {waiting.seconds // (60 * 60) % 24} hours, {waiting.seconds // (60) % 60} minutes and {waiting.seconds % 60} {ending}\n"
+            msg = f"There are {waiting.days} day(s), {waiting.seconds // (60 * 60) % 24} hour(s), {waiting.seconds // (60) % 60} minutes and {waiting.seconds % 60} second(s) {ending}\n"
 
         return msg
     
@@ -36,6 +36,19 @@ class Competitions(commands.Cog):
 
         msg = self.get_time_diff(event_time, "until we need a bigger boat...")
         
+        await ctx.send(msg)
+
+    @commands.command(name="defcon")
+    async def defcon_command(self, ctx):
+
+        quals_time = datetime.datetime(year = 2020, month = 5, day = 16, hour = 0)
+        finals_time = datetime.datetime(year = 2020, month = 8, day = 6, hour = 0)
+        
+        quals_msg = self.get_time_diff(quals_time, "until we get to qualify (or die of exhaustion in the process)")
+        finals_msg = self.get_time_diff(finals_time, "until ~~nuclear~~ cyber armageddon")
+
+        msg = quals_msg + finals_msg
+
         await ctx.send(msg)
 
     
